e33492ffb550669fd2d0fc24701124c8a3b0b8f0
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2021-10-23 15:45:36 -0300

    feat: Definição das actions referente ao CRUD
    Criado por: Phelipe Matos (PHMATOS07)

---
+++ src/app/store/crud.actions.ts
@@ -0,0 +1,6 @@
+import { createAction } from '@ngrx/store';
+
+export const CREATE = createAction('[CRUD] Create');
+export const READ = createAction('[CRUD] Read');
+export const UPDATE = createAction('[CRUD] Update');
+export const DELETE = createAction('[CRUD] Delete');
