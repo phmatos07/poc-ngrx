f01748317e87cd465c80b8eaa5f67e9796913ef1
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2021-10-26 16:57:42 -0300

    feat: Criação do serviço 'CrudFacade' e atualização do 'Crud.Action'

--- src/app/store/crud.actions.ts
+++ src/app/store/crud.actions.ts
@@ -1,6 +1,7 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
+import { User } from './../models/user.interface';
 
-export const CREATE = createAction('[CRUD] Create');
+export const CREATE = createAction('[CRUD] Create', props<User>());
 export const READ = createAction('[CRUD] Read');
 export const UPDATE = createAction('[CRUD] Update');
 export const DELETE = createAction('[CRUD] Delete');
---
+++ src/app/store/crud.facade.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { User } from './../models/user.interface';
+import * as CrudActions from './crud.actions';
+
+@Injectable()
+export class CrudFacade {
+
+  constructor(
+    private readonly store: Store
+  ) { }
+
+  create(user: User): void {
+    this.store.dispatch(CrudActions.CREATE(user));
+  }
+}
