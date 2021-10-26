52472c1e2bb400e0c04d28d066bf500f409ae00f
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2021-10-26 10:39:45 -0300

    feat: Criação do formulário reativo para cadastro de usuário e estilização usando o Material Design
    Criado por: Phelipe Matos (PHMATOS07)

--- src/app/crud/create/create.component.ts
+++ src/app/crud/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -7,7 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  userRegistration = new FormGroup({});
+  displaysPassword = false;
 
-  ngOnInit() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.startForm();
+  }
+
+  onSubmit(): void {
+
+  }
+
+  hidePassword(): void {
+    this.displaysPassword = !this.displaysPassword;
+  }
+
+  private startForm(): void {
+    this.userRegistration = this.formBuilder.group({
+      name: ['', [
+        Validators.required,
+        Validators.maxLength(50),
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.maxLength(100),
+        Validators.email
+      ]],
+      cellPhone: ['', [
+        Validators.required,
+        Validators.maxLength(11)
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.maxLength(50)
+      ]]
+    });
+  }
 }
--- src/app/crud/create/create.module.ts
+++ src/app/crud/create/create.module.ts
@@ -1,13 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { CreateRoutingModule } from './create-routing.module';
 import { CreateComponent } from './create.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
     CreateRoutingModule
   ],
   exports: [CreateComponent],
