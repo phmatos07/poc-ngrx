cc4ae552ac2ae46f321b70545b1b453936802373
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2021-11-24 17:55:20 -0300

    feat: Criação e implementação do método 'currentDate'

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -3,5 +3,5 @@ export class User {
   email!: string;
   cellPhone!: number;
   password!: string;
-  dateStamp?: number;
+  dateStamp?: string;
 }
--- src/app/store/crud.helpers.ts
+++ src/app/store/crud.helpers.ts
@@ -8,9 +8,17 @@ export class CrudHelpers {
   static addValue(state: ProfilesState, user: User): ProfilesState {
     if (Array.isArray(state.users) && state.users.length >= 1) {
       const users = Object.assign([], state.users);
-      users.push({ ...user, dateStamp: new Date().getDate() });
+      users.push({ ...user, dateStamp: CrudHelpers.currentDate() });
       return { ...state, users };
     }
-    return { ...state, users: [{ ...user, dateStamp: new Date().getDate() }] };
+    return { ...state, users: [{ ...user, dateStamp: CrudHelpers.currentDate() }] };
+  }
+
+  private static currentDate(): string {
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${year}-${month}-${day}`;
   }
 }
