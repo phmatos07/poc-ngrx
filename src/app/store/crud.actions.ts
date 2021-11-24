cfbb486a73738f7615a3cbdc61f78193e8023ccd
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2021-11-24 15:26:27 -0300

    feat: Atualização das actions

--- src/app/store/crud.actions.ts
+++ src/app/store/crud.actions.ts
@@ -1,7 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 import { User } from '../models/user.model';
 
+export const INIT = createAction('[CRUD] Init');
 export const CREATE = createAction('[CRUD] Create', props<User>());
-export const READ = createAction('[CRUD] Read');
 export const UPDATE = createAction('[CRUD] Update');
 export const DELETE = createAction('[CRUD] Delete');
