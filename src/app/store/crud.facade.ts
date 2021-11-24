16d66716ae5d55101c75947d10d846c8313525a1
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2021-11-24 15:33:28 -0300

    feat: Criação dos selectors e implementação do selector 'users' na class 'CrudFacade'

--- src/app/store/crud.facade.ts
+++ src/app/store/crud.facade.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { User } from '../models/user.model';
+import { select, Store } from '@ngrx/store';
+import { User } from 'src/app/models/user.model';
 import * as CrudActions from './crud.actions';
+import * as CrudSelectors from './crud.selectors';
 
 @Injectable()
 export class CrudFacade {
+
+  users$ = this.store.pipe(select(CrudSelectors.users));
 
   constructor(
     private readonly store: Store
