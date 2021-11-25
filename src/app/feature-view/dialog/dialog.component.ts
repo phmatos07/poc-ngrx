644028089397ce74ec5fd6388e19dee9230cc86a
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2021-11-25 17:17:58 -0300

    feat: Atualização das informações referente ao 'ActionType' e implementaçã dos mesmo no componente 'DialogComponent'

--- src/app/feature-view/dialog/dialog.component.ts
+++ src/app/feature-view/dialog/dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActionType } from './model/action-type.enum';
 import { DialogData } from './model/dialog-data.model';
 import { DialogType } from './model/dialog-type.enum';
 
@@ -11,13 +12,14 @@ import { DialogType } from './model/dialog-type.enum';
 export class DialogComponent {
 
   readonly DIALOG_TYPE = DialogType;
+  readonly ACTION_TYPE = ActionType;
 
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
   ) { }
 
-  onAction(actionType: 'CLOSE' | 'OK'): void {
+  onAction(actionType: ActionType): void {
     this.dialogRef.close(actionType);
   }
 }
