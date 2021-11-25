416890dd2b73c19a3a46d5f914c990fb9020d5b8
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2021-11-25 17:18:49 -0300

    feat: Implementação do módulos 'MatDialogModule' e 'DialogModule' no componente 'DeleteComponent'

--- src/app/crud/delete/delete.component.ts
+++ src/app/crud/delete/delete.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user.model';
+import { DialogComponent } from './../../feature-view/dialog/dialog.component';
+import { ActionType } from './../../feature-view/dialog/model/action-type.enum';
+import { DialogData } from './../../feature-view/dialog/model/dialog-data.model';
+import { DialogType } from './../../feature-view/dialog/model/dialog-type.enum';
 import { CrudFacade } from './../../store/crud.facade';
 
 @Component({
@@ -17,7 +22,8 @@ export class DeleteComponent implements OnInit {
   ]);
 
   constructor(
-    private crud: CrudFacade
+    private crud: CrudFacade,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
@@ -26,7 +32,18 @@ export class DeleteComponent implements OnInit {
 
   delete(): void {
     if (this.isSelectValid) {
-      this.crud.delete(this.selectUsers.value);
+
+      const dialogRef = this.dialog.open(DialogComponent, {
+        data: new DialogData(DialogType.CONFIRM, 'Tem certeza que deseja excluir esse perfil?'),
+      });
+
+      dialogRef.afterClosed()
+        .subscribe((action: ActionType) => {
+          if (action === ActionType.OK) {
+            this.crud.delete(this.selectUsers.value);
+            this.selectUsers.reset();
+          }
+        });
     }
   }
 
--- src/app/crud/delete/delete.module.ts
+++ src/app/crud/delete/delete.module.ts
@@ -2,8 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatSelectModule } from '@angular/material/select';
 import { RouterModule } from '@angular/router';
+import { DialogModule } from './../../feature-view/dialog/dialog.module';
 import { DeleteRoutingModule } from './delete-routing.module';
 import { DeleteComponent } from './delete.component';
 
@@ -14,7 +16,9 @@ import { DeleteComponent } from './delete.component';
     DeleteRoutingModule,
     ReactiveFormsModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule,
+    DialogModule
   ],
   exports: [DeleteComponent],
   declarations: [DeleteComponent],
