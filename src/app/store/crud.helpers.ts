613177b19d6658a29de6bdab136d9379d3a09ab4
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2021-11-26 23:19:04 -0300

    fix: Atualização do método 'currentDate' para obter a Data no formato brasileiro

--- src/app/store/crud.helpers.ts
+++ src/app/store/crud.helpers.ts
@@ -37,6 +37,6 @@ export class CrudHelpers {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
-    return `${year}-${month}-${day}`;
+    return `${day}/${month}/${year}`;
   }
 }
