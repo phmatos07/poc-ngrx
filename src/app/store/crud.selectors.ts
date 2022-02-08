c201dd9059a7a2bd2d4c101af75aecbe4c41b767
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2022-02-08 15:33:46 -0300

    feat: Implementando constantes nativas do @ngrx/entity para obter dados referente as Entidades
    Implementado por: Phelipe Matos (PHMATOS07)

--- src/app/store/crud.selectors.ts
+++ src/app/store/crud.selectors.ts
@@ -1,6 +1,18 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { KEY_CRUD } from './../models/key-crud.const';
 import { ProfilesState } from './../models/profiles-state.interface';
+import { adapter } from './crud.reducer';
+
+const {
+  selectIds,
+  selectEntities,
+  selectAll,
+  selectTotal
+} = adapter.getSelectors();
 
 export const profilesState = createFeatureSelector<ProfilesState>(KEY_CRUD);
-export const users = createSelector(profilesState, (state: ProfilesState) => state);
+export const fullState = createSelector(profilesState, (state: ProfilesState) => state);
+export const userIds = createSelector(profilesState, selectIds);
+export const userEntities = createSelector(profilesState, selectEntities);
+export const allUsers = createSelector(profilesState, selectAll);
+export const userTotal = createSelector(profilesState, selectTotal);
