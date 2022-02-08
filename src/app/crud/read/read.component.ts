8959843c944d0a464f2878995f55359f2aeeeff9
PHELIPE RIBEIRO DE MATOS <phelipe.matos@viavarejo.com.br>
2022-02-08 15:35:35 -0300

    feat: Atualizando referencias de 2 selects no Crud.Facade e também no componente de Read para renderizar as informações na tela
    Atualizado por: Phelipe Matos (PHMATOS07)

--- src/app/crud/read/read.component.ts
+++ src/app/crud/read/read.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
 import { User } from '../../models/user.interface';
 import { CrudFacade } from './../../store/crud.facade';
 
@@ -9,17 +10,21 @@ import { CrudFacade } from './../../store/crud.facade';
 })
 export class ReadComponent implements OnInit {
 
-  users?: User[] | null;
+  users?: User[];
 
   constructor(
     private crud: CrudFacade
   ) { }
 
   ngOnInit(): void {
-    /* this.crud.users$
+    this.getAllUsers();
+  }
+
+  private getAllUsers(): void {
+    this.crud.allUsers$
       .pipe(first())
-      .subscribe((users: User[] | null) => this.users = users,
+      .subscribe((users: User[]) => this.users = users,
         error => console.error(error)
-      ); */
+      );
   }
 }
--- src/app/store/crud.facade.ts
+++ src/app/store/crud.facade.ts
@@ -1,22 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { User } from 'src/app/models/user.interface';
 import * as CrudActions from './crud.actions';
+import * as CrudSelectors from './crud.selectors';
 
 @Injectable()
 export class CrudFacade {
 
-  /* users$ = this.store.pipe(
-    select(CrudSelectors.users),
-    map(users => users)
-  ); */
+  fullState$ = this.store.pipe(select(CrudSelectors.fullState));
+  allUsers$ = this.store.pipe(select(CrudSelectors.allUsers));
 
   constructor(
     private readonly store: Store
   ) { }
 
   create(user: User): void {
-    this.store.dispatch(CrudActions.CREATE({user}));
+    this.store.dispatch(CrudActions.CREATE({ user }));
   }
 
   update(idUser: number, user: User): void {
